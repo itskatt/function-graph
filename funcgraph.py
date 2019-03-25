474bc9df9069bbd502d24b4b80d43bc8d69ee9ef
itskatt <raph10@mail.com>
2019-03-25 19:05:13 +0100

    Add a verbose flag.

--- funcgraph.py
+++ funcgraph.py
@@ -16,7 +16,7 @@ class Orthogonal():
     """
 
     def __init__(self, expression, size=500, mode="static", graduation=None, loop=0, save_to_buffer=False,
-                 filename="graph"):
+                 filename="graph", verbose=False):
         self.width = round(size)
         self.height = round(size)
         self.center = (round(size / 2), round(size / 2))
@@ -28,6 +28,7 @@ class Orthogonal():
         self._loop = loop
         self._save_to_buff = save_to_buffer
         self._filename = filename
+        self._verb = verbose
 
         self._frames = []
         self._coord_list = []
@@ -43,6 +44,10 @@ class Orthogonal():
         start = time.perf_counter()
         yield
         self.stats[val] = time.perf_counter() - start
+
+    def log(self, text, end="\n"):
+        if self._verb:
+            print(text, end=end)
 
     def _coords(self, x, y):
         """
@@ -77,6 +82,7 @@ class Orthogonal():
             "black",
             self.width // 100
         )
+        self.log("Horizontal and vertical lines drewn.")
         # We don't draw the graduation if there is no graduation and
         # if it will look bad
         if not self._graduation:
@@ -109,6 +115,7 @@ class Orthogonal():
                 "black",
                 self.height // 250
             )
+        self.log("Graduation drewn.\n")
 
     def _calculate_coords(self):
         if self._graduation:
@@ -150,10 +157,12 @@ class Orthogonal():
             env.update({"x": x / a})
             try:
                 y = eval(self.expr, env)
-            except (ArithmeticError, ValueError):
+            except (ArithmeticError, ValueError) as e:
                 self._coord_list.append(None)
+                self.log(f"Failed to calculate expression with {x}: {e}")
             else:
                 self._coord_list.append(self._coords(x, y * a))
+        self.log("")  # Space
 
     def _draw_graph_line(self, bg, draw, size):
 
@@ -167,8 +176,10 @@ class Orthogonal():
                     if c[1] < self.height and line_end[1] > self.height:
                         pass
                     else:
+                        self.log(f"Will not draw coords from {c} to {line_end}")
                         continue
                 except TypeError:
+                    self.log(f"Will not draw coords from {c} to {line_end}")
                     continue
             try:
                 draw.line(
@@ -176,19 +187,21 @@ class Orthogonal():
                     "red",  # Color change
                     size
                 )
-            except OverflowError:
-                # An OverflowError was raised (ex: Python int too large
-                # to convert to C long)
-                pass
+            except OverflowError as e:
+                self.log(f"Failed to draw coords from {c} to {line_end}: {e}")
             else:
                 if self.mode == "animated":
                     self._frames.append(bg.copy())
+        self.log("")
 
     def draw_graph(self):
         """
         Calculates the coordinates, draws the graph and saves it
         """
-
+        self.log(
+            f"Preparing to draw a graph wide of {self.width} pixels, with {self._graduation} "
+            f"as graduation, using {self.expr}...\n"
+        )
         backgroud = Image.new("RGB", (self.width, self.height), "white")
         draw = ImageDraw.Draw(backgroud)
 
@@ -203,6 +216,7 @@ class Orthogonal():
         buffer = io.BytesIO()
 
         if self.mode == "animated":
+            self.log("Now saving animated graph...")
             with self._mesure_time("save_time"):
                 backgroud.save(
                     f"{self._filename}.gif" if not self._save_to_buff else buffer,
@@ -212,17 +226,17 @@ class Orthogonal():
                     duration=20,
                     loop=self._loop
                 )
-                buffer.seek(0)
-                self.buffer = buffer
-            return
+        else:
+            with self._mesure_time("save_time"):
+                self.log("Now saving graph...")
+                backgroud.save(
+                    f"{self._filename}.png" if not self._save_to_buff else buffer,
+                    format="PNG"
+                )
+        buffer.seek(0)
+        self.buffer = buffer
 
-        with self._mesure_time("save_time"):
-            backgroud.save(
-                f"{self._filename}.png" if not self._save_to_buff else buffer,
-                format="PNG"
-            )
-            buffer.seek(0)
-            self.buffer = buffer
+        self.log("Done.")
 
 
 def get_cli_args():
@@ -248,9 +262,16 @@ def get_cli_args():
         default=10
     )
     p.add_argument(
+        "-v",
+        "--verbose",
+        help="Give more output while generating the graph.",
+        action="store_true",
+        default=False
+    )
+    p.add_argument(
         "-f",
         "--filename",
-        help="The output filename. The extention is automaticly added",
+        help="The output filename. The extention is automaticly added.",
         default="graph"
     )
     p.add_argument(
@@ -291,7 +312,8 @@ def main():
         args.size,
         graduation=args.graduation,
         mode="animated" if args.animated else "static",
-        filename=args.filename
+        filename=args.filename,
+        verbose=args.verbose
     )
     ortho.draw_graph()
     if args.time:
